97498df77db8482e7045058412a9ff65a829cf23
Noha <noha.ben254@gmail.com>
2025-04-17 14:02:02 +0200

    init

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+
+    $users = \App\Models\User::all();
+    return Inertia::render('welcome', [
+        'phpVersion' => PHP_VERSION,
+    ]);
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
