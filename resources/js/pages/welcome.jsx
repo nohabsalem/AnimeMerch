f4bdbfcdd59dc0788e599f64405319220ce6d89a
Noha <noha.ben254@gmail.com>
2025-04-20 18:39:09 +0200

    better structure

--- resources/js/pages/welcome.jsx
+++ resources/js/pages/welcome.jsx
@@ -1,5 +1,8 @@
 import { Head, Link, usePage } from '@inertiajs/react';
+import Banner from "../assets/img/jojo-banner.jpg"
 
+import ProductCards from '../products/cards';
+import Footer from "../components/footer"
 export default function Welcome({ phpVersion }) {
     const { auth } = usePage().props;
 
@@ -9,43 +12,51 @@ export default function Welcome({ phpVersion }) {
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
-            <div className="flex min-h-screen flex-col items-center p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
-                {/* <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl"> */}
-                <header className="w-full bg-[#6C3989] text-white py-3 px-6">
+            {/* <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl"> */}
+            <header className="w-full bg-[#6C3989] text-white py-3 px-6">
 
-                    <nav className="flex items-center justify-end gap-4 bg-[#6C3989]">
-                        {auth.user ? (
+                <nav className="flex items-center justify-end gap-4 bg-[#6C3989]">
+                    {auth.user ? (
+                        <Link
+                            href={route('dashboard')}
+                            className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                        >
+                            Dashboard
+                        </Link>
+                    ) : (
+                        <>
                             <Link
-                                href={route('dashboard')}
-                                className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                                href={route('login')}
+                                className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
                             >
-                                Dashboard
+                                Log in
                             </Link>
-                        ) : (
-                            <>
-                                <Link
-                                    href={route('login')}
-                                    className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
-                                >
-                                    Log in
-                                </Link>
-                                <Link
-                                    href={route('register')}
-                                    className="inline-block rounded-sm border border-white px-5 py-1.5 text-sm leading-normal text-white hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                                >
-                                    Register
-                                </Link>
-                            </>
-                        )}
-                    </nav>
-                </header>
-                <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
-                    <main>
-                        <p>main</p>
-                    </main>
-                </div>
-                <div className="hidden h-14.5 lg:block"></div>
+                            <Link
+                                href={route('register')}
+                                className="inline-block rounded-sm border border-white px-5 py-1.5 text-sm leading-normal text-white hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </nav>
+            </header>
+            <div className="w-full flex items-center justify-center">
+                <img
+                    src={Banner}
+                    className="w-full object-cover max-h-[350px]"
+                    alt="Bannière"
+                />
+
             </div>
+
+            <main className="flex flex-col items-center justify-center gap-4 px-6 py-8">
+                <h1 className="text-4xl font-bold text-[#6C3989]">Bienvenue sur AnimeMerch</h1>
+                <p className="text-lg text-gray-700">Votre destination ultime pour les produits dérivés d'animés !</p>
+                < ProductCards />
+            </main>
+
+            < Footer />
         </>
     );
 }
