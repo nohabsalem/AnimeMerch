b3a8fbc5982fdff80cea3b16d85df74b28dfead5
Noha <noha.ben254@gmail.com>
2025-04-20 18:38:30 +0200

    links + style

---
+++ resources/js/components/footer.jsx
@@ -0,0 +1,27 @@
+import { Link } from '@inertiajs/react';
+
+export default function Footer() {
+    return (
+        <footer className="w-full bg-[#6C3989] text-white py-5 px-6">
+            <div className="flex items-center justify-center gap-4">
+                {/* <a href="/privacy" className="text-sm hover:underline">Politique de confidentialité</a>
+                <a href="/terms" className="text-sm hover:underline">Conditions d'utilisation</a> * */}
+
+                <Link to="/" className="text-sm hover:underline mb-2 sm:mb-0">
+                    Accueil
+                </Link>
+                <Link to="/about" className="text-sm hover:underline mb-2 sm:mb-0">
+                    À propos de nous
+                </Link>
+                <Link to="/privacy-policy" className="text-sm hover:underline mb-2 sm:mb-0">
+                    Politique de Confidentialité
+                </Link>
+                <Link to="/terms-and-conditions" className="text-sm hover:underline mb-2 sm:mb-0">
+                    CGV
+                </Link>
+                <p className="text-sm">© 2025 AnimeMerch. Tous droits réservés.</p>
+
+            </div>
+        </footer>
+    );
+}
