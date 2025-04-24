e942a88b94ef76313391bb9b83e68c7cfaf68ea9
Noha <noha.ben254@gmail.com>
2025-04-24 13:18:23 +0200

    purple border

--- resources/js/pages/auth/login.jsx
+++ resources/js/pages/auth/login.jsx
@@ -9,6 +9,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
 
+import Wallpaper from "../../assets/img/jolyne-wallpaper.jpg";
 export default function Login({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
         email: '',
@@ -22,77 +23,76 @@ export default function Login({ status, canResetPassword }) {
             onFinish: () => reset('password'),
         });
     };
-
     return (
         <AuthLayout title="Connectez-vous à votre compte" description="Entrez vos identifiants pour vous connecter.">
             <Head title="Log in" />
-
-            <form className="flex flex-col gap-6 border-2 border-[#6C3989] p-6 rounded-2xl" onSubmit={submit}>
-                <div className="grid gap-6">
-                    <div className="grid gap-2">
-                        <Label htmlFor="email">Adresse mail :</Label>
-                        <Input
-                            id="email"
-                            type="email"
-                            required
-                            autoFocus
-                            tabIndex={1}
-                            autoComplete="email"
-                            value={data.email}
-                            onChange={(e) => setData('email', e.target.value)}
-                            placeholder="email@example.com"
-                        />
-                        <InputError message={errors.email} />
-                    </div>
-
-                    <div className="grid gap-2">
-                        <div className="flex items-center">
-                            <Label htmlFor="password">Mot de passe :</Label>
-                            {canResetPassword && (
-                                <TextLink href={route('password.request')} className="ml-auto text-sm" tabIndex={5}>
-                                    Mot de passe oublié ?
-                                </TextLink>
-                            )}
+            <div className="bg-[url('../../assets/img/jolyne-wallpaper.jpg')] bg-cover bg-center h-64 w-full">
+                <form className="flex flex-col gap-6 border-2 border-[#6C3989] p-6 rounded-2xl" onSubmit={submit}>
+                    <div className="grid gap-6">
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Adresse mail :</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                required
+                                autoFocus
+                                tabIndex={1}
+                                autoComplete="email"
+                                value={data.email}
+                                onChange={(e) => setData('email', e.target.value)}
+                                placeholder="email@example.com"
+                            />
+                            <InputError message={errors.email} />
                         </div>
-                        <Input
-                            id="password"
-                            type="password"
-                            required
-                            tabIndex={2}
-                            autoComplete="current-password"
-                            value={data.password}
-                            onChange={(e) => setData('password', e.target.value)}
-                            placeholder="Password"
-                        />
-                        <InputError message={errors.password} />
+
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Mot de passe :</Label>
+                                {canResetPassword && (
+                                    <TextLink href={route('password.request')} className="ml-auto text-sm text-[#6C3989]" tabIndex={5} >
+                                        Mot de passe oublié ?
+                                    </TextLink>
+                                )}
+                            </div>
+                            <Input
+                                id="password"
+                                type="password"
+                                required
+                                tabIndex={2}
+                                autoComplete="current-password"
+                                value={data.password}
+                                onChange={(e) => setData('password', e.target.value)}
+                                placeholder="Password"
+                            />
+                            <InputError message={errors.password} />
+                        </div>
+
+                        <div className="flex items-center space-x-3">
+                            <Checkbox
+                                id="remember"
+                                name="remember"
+                                checked={data.remember}
+                                onClick={() => setData('remember', !data.remember)}
+                                tabIndex={3}
+                                className="data-[state=checked]:bg-[#6C3989] data-[state=checked]:border-[#]"
+                            />
+                            <Label htmlFor="remember">Remember me</Label>
+                        </div>
+
+                        <Button type="submit" className="mt-4 w-full bg-[#FF39B7] hover:bg-[#FF39B7]" tabIndex={4} disabled={processing}>
+                            {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
+                            Log in
+                        </Button>
                     </div>
 
-                    <div className="flex items-center space-x-3">
-                        <Checkbox
-                            id="remember"
-                            name="remember"
-                            checked={data.remember}
-                            onClick={() => setData('remember', !data.remember)}
-                            tabIndex={3}
-                            className="data-[state=checked]:bg-[#6C3989] data-[state=checked]:border-[#]"
-                        />
-                        <Label htmlFor="remember">Remember me</Label>
+                    <div className="text-muted-foreground text-center text-sm">
+                        Vous n'avez pas de compte ?{' '}
+                        <TextLink href={route('register')} tabIndex={5} className="text-[#6C3989]">
+                            Inscrivez-vous
+                        </TextLink>
                     </div>
-
-                    <Button type="submit" className="mt-4 w-full bg-[#FF39B7] hover:bg-[#FF39B7]" tabIndex={4} disabled={processing}>
-                        {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-                        Log in
-                    </Button>
-                </div>
-
-                <div className="text-muted-foreground text-center text-sm">
-                    Vous n'avez pas de compte ?{' '}
-                    <TextLink href={route('register')} tabIndex={5} className="text-[#6C3989]">
-                        Inscrivez-vous
-                    </TextLink>
-                </div>
-            </form>
-
+                </form>
+            </div>
             {status && <div className="mb-4 text-center text-sm font-medium text-green-600">{status}</div>}
         </AuthLayout>
     );
