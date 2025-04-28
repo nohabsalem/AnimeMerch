cc1f96615367628ece36c0a70cdb0bde4203d7d5
Noha <noha.ben254@gmail.com>
2025-04-28 13:36:13 +0200

    colors

--- resources/js/pages/dashboard.jsx
+++ resources/js/pages/dashboard.jsx
@@ -1,7 +1,9 @@
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { Head } from '@inertiajs/react';
+import { Link } from '@inertiajs/react';
 
+import imgExample from "../assets/img/square.png";
 const breadcrumbs = [
     {
         title: 'Dashboard',
@@ -15,20 +17,38 @@ export default function Dashboard() {
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className="dark:border-sidebar-border relative flex items-center overflow-hidden rounded-xl border-2 border-[#6C3989] p-4">
+                        <div className="flex-shrink-0">
+                            <img src={imgExample} alt="" className="h-12 w-12" />
+                        </div>
+                        <div className="ml-4">
+                            <Link href="/commandes" className="font-semibold text-lg">Vos commandes </Link>
+                            <p className="text-sm text-gray-600">Suivre, retourner ou acheter à nouveau.</p>
+                        </div>
                     </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+
+                    <div className="dark:border-sidebar-border relative flex items-center overflow-hidden rounded-xl border-2 border-[#6C3989] p-4">
+                        <div className="flex-shrink-0">
+                            <img src={imgExample} alt="" className="h-12 w-12" />
+                        </div>
+                        <div className="ml-4">
+                            <Link href="/cordonnées" className="font-semibold text-lg">Adresses  </Link>
+                            <p className="text-sm text-gray-600">Modifier les adresses et les préférences de livraison des commandes.</p>
+                        </div>
                     </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+
+
+                    <div className="dark:border-sidebar-border relative flex items-center overflow-hidden rounded-xl border-2 border-[#6C3989] p-4">
+                        <div className="flex-shrink-0">
+                            <img src={imgExample} alt="" className="h-12 w-12" />
+                        </div>
+                        <div className="ml-4">
+                            <Link href="/paiements" className="font-semibold text-lg">Vos paiements  </Link>
+                            <p className="text-sm text-gray-600">Afficher toutes les transactions, gérer les modes de paiement et les paramètres.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                </div>
-            </div>
-        </AppLayout>
+                </div >
+            </div >
+        </AppLayout >
     );
 }
