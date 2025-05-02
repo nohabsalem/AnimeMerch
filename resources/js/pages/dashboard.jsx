9e30f7ba2d8b32f0a47a293c4c7944bbd3987748
Noha <noha.ben254@gmail.com>
2025-05-02 16:11:37 +0200

    footer

--- resources/js/pages/dashboard.jsx
+++ resources/js/pages/dashboard.jsx
@@ -1,9 +1,7 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
-import { Head } from '@inertiajs/react';
-import { Link } from '@inertiajs/react';
-
-import imgExample from "../assets/img/square.png";
+import { Head, Link } from '@inertiajs/react';
+import imgExample from '../assets/img/square.png';
+import Footer from '../components/footer';
 const breadcrumbs = [
     {
         title: 'Dashboard',
@@ -13,42 +11,52 @@ const breadcrumbs = [
 
 export default function Dashboard() {
     return (
-        <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Dashboard" />
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="dark:border-sidebar-border relative flex items-center overflow-hidden rounded-xl border-2 border-[#6C3989] p-4">
-                        <div className="flex-shrink-0">
-                            <img src={imgExample} alt="" className="h-12 w-12" />
+        <>
+            <AppLayout breadcrumbs={breadcrumbs}>
+                <Head title="Dashboard" />
+                <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                        <div className="dark:border-sidebar-border relative flex items-center overflow-hidden rounded-xl border-2 border-[#6C3989] p-4">
+                            <div className="flex-shrink-0">
+                                <img src={imgExample} alt="" className="h-12 w-12" />
+                            </div>
+                            <div className="ml-4">
+                                <Link href="/commandes" className="text-lg font-semibold">
+                                    Vos commandes{' '}
+                                </Link>
+                                <p className="text-sm text-gray-600">Suivre, retourner ou acheter à nouveau.</p>
+                            </div>
                         </div>
-                        <div className="ml-4">
-                            <Link href="/commandes" className="font-semibold text-lg">Vos commandes </Link>
-                            <p className="text-sm text-gray-600">Suivre, retourner ou acheter à nouveau.</p>
+
+                        <div className="dark:border-sidebar-border relative flex items-center overflow-hidden rounded-xl border-2 border-[#6C3989] p-4">
+                            <div className="flex-shrink-0">
+                                <img src={imgExample} alt="" className="h-12 w-12" />
+                            </div>
+                            <div className="ml-4">
+                                <Link href="/cordonnées" className="text-lg font-semibold">
+                                    Adresses{' '}
+                                </Link>
+                                <p className="text-sm text-gray-600">Modifier les adresses et les préférences de livraison des commandes.</p>
+                            </div>
+                        </div>
+
+                        <div className="dark:border-sidebar-border relative flex items-center overflow-hidden rounded-xl border-2 border-[#6C3989] p-4">
+                            <div className="flex-shrink-0">
+                                <img src={imgExample} alt="" className="h-12 w-12" />
+                            </div>
+                            <div className="ml-4">
+                                <Link href="/paiements" className="text-lg font-semibold">
+                                    Vos paiements{' '}
+                                </Link>
+                                <p className="text-sm text-gray-600">
+                                    Afficher toutes les transactions, gérer les modes de paiement et les paramètres.
+                                </p>
+                            </div>
                         </div>
                     </div>
-
-                    <div className="dark:border-sidebar-border relative flex items-center overflow-hidden rounded-xl border-2 border-[#6C3989] p-4">
-                        <div className="flex-shrink-0">
-                            <img src={imgExample} alt="" className="h-12 w-12" />
-                        </div>
-                        <div className="ml-4">
-                            <Link href="/cordonnées" className="font-semibold text-lg">Adresses  </Link>
-                            <p className="text-sm text-gray-600">Modifier les adresses et les préférences de livraison des commandes.</p>
-                        </div>
-                    </div>
-
-
-                    <div className="dark:border-sidebar-border relative flex items-center overflow-hidden rounded-xl border-2 border-[#6C3989] p-4">
-                        <div className="flex-shrink-0">
-                            <img src={imgExample} alt="" className="h-12 w-12" />
-                        </div>
-                        <div className="ml-4">
-                            <Link href="/paiements" className="font-semibold text-lg">Vos paiements  </Link>
-                            <p className="text-sm text-gray-600">Afficher toutes les transactions, gérer les modes de paiement et les paramètres.</p>
-                        </div>
-                    </div>
-                </div >
-            </div >
-        </AppLayout >
+                </div>
+            </AppLayout>
+            <Footer />
+        </>
     );
 }
