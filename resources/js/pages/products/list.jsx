646d71db8626d5230efe027dec10e6fee1c3ffb7
Noha <noha.ben254@gmail.com>
2025-05-07 15:21:10 +0200

    new components

--- resources/js/pages/products/list.jsx
+++ resources/js/pages/products/list.jsx
@@ -1,8 +1,10 @@
+import PriceFilter from '../../components/filters/price';
+import SizeFilter from '../../components/filters/size';
+import ToggleFilters from '../../components/filters/toggle';
 import Footer from '../../components/footer';
 import Header from '../../components/header';
 import Searchbar from '../../components/searchbar';
 import ProductCards from './cards';
-
 export default function ProductList() {
     return (
         <div className="flex min-h-screen flex-col">
@@ -12,6 +14,11 @@ export default function ProductList() {
                 <h1 className="text-4xl font-bold text-[#6C3989]">Liste des produits :</h1>
                 <Searchbar />
                 <div className="flex w-full justify-center">
+                    <div className="flex w-1/4 flex-col gap-6">
+                        <ToggleFilters />
+                        <PriceFilter />
+                        <SizeFilter />
+                    </div>
                     <ProductCards />
                 </div>
             </main>
