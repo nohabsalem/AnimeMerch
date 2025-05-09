9bfa3bd03584869370b2a3cb962e92673ed86791
Noha <noha.ben254@gmail.com>
2025-05-09 10:52:45 +0200

    nav to home

--- resources/js/components/header.jsx
+++ resources/js/components/header.jsx
@@ -1,51 +1,56 @@
 import { Link, usePage } from '@inertiajs/react';
-
+import square from '../assets/img/square.png';
 export default function Header() {
     const { auth } = usePage().props;
 
     return (
         <header className="w-full bg-[#6C3989] px-6 py-3 text-white">
-            <nav className="flex items-center justify-end gap-4 bg-[#6C3989]">
-                {auth.user ? (
-                    <>
-                        <Link
-                            href={route('cart')}
-                            className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#fff] dark:text-[#EDEDEC]"
-                        >
-                            Panier
-                        </Link>
+            <div className="flex items-center justify-between">
+                <Link href="/">
+                    <img src={square} alt="Logo" className="h-10 w-auto" />
+                </Link>
 
-                        <Link
-                            href={route('dashboard')}
-                            // className="text-[#1b1b18]dark:text-[#EDEDEC] inline-block rounded-sm border px-5 py-1.5 text-sm leading-normal"
-                            className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#fff] dark:text-[#EDEDEC]"
-                        >
-                            Mon compte
-                        </Link>
-                    </>
-                ) : (
-                    <>
-                        <Link
-                            href={route('cart')}
-                            className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#fff] dark:text-[#EDEDEC]"
-                        >
-                            Panier
-                        </Link>
-                        <Link
-                            href={route('login')}
-                            className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#fff] dark:text-[#EDEDEC]"
-                        >
-                            Log in
-                        </Link>
-                        <Link
-                            href={route('register')}
-                            className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#fff] dark:text-[#EDEDEC]"
-                        >
-                            Register
-                        </Link>
-                    </>
-                )}
-            </nav>
+                <nav className="flex items-center justify-end gap-4 bg-[#6C3989]">
+                    {auth.user ? (
+                        <>
+                            <Link
+                                href={route('cart')}
+                                className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#fff] dark:text-[#EDEDEC]"
+                            >
+                                Panier
+                            </Link>
+
+                            <Link
+                                href={route('dashboard')}
+                                className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#fff] dark:text-[#EDEDEC]"
+                            >
+                                Mon compte
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link
+                                href={route('cart')}
+                                className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#fff] dark:text-[#EDEDEC]"
+                            >
+                                Panier
+                            </Link>
+                            <Link
+                                href={route('login')}
+                                className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#fff] dark:text-[#EDEDEC]"
+                            >
+                                Log in
+                            </Link>
+                            <Link
+                                href={route('register')}
+                                className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#fff] dark:text-[#EDEDEC]"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </nav>
+            </div>
         </header>
     );
 }
