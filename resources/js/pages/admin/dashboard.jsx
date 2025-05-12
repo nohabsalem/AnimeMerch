8e59538439fde5df56f501628f44160e53841f4f
Noha <noha.ben254@gmail.com>
2025-05-12 16:01:57 +0200

    removal comments

--- resources/js/pages/admin/dashboard.jsx
+++ resources/js/pages/admin/dashboard.jsx
@@ -1,30 +1,5 @@
-// import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
-// import AppLayout from '@/layouts/app-layout';
-// import { Head } from '@inertiajs/react';
-// import { Link } from '@inertiajs/react';
-
-// const breadcrumbs = [
-//     {
-//         title: 'Dashboard',
-//         href: '/dashboard',
-//     },
-// ];
-
-// export default function AdminDashboard() {
-//     return (
-//         <>
-//             <AppLayout breadcrumbs={breadcrumbs}>
-//                 <Head title="Admin" />
-//                 <p>Admin Dashboard</p>
-//             </AppLayout >
-//         </>
-//     );
-// }
-
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { Head } from '@inertiajs/react';
-import { Link } from '@inertiajs/react';
 
 const breadcrumbs = [
     {
