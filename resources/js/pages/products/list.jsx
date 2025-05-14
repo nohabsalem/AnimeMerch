db2043c0ae5fea3f5d738b87125ddcdbbeadf389
Noha <noha.ben254@gmail.com>
2025-05-14 14:03:45 +0200

    responsive

--- resources/js/pages/products/list.jsx
+++ resources/js/pages/products/list.jsx
@@ -31,9 +31,8 @@ export default function ProductList() {
 
             <main className="flex flex-grow flex-col items-center gap-4 px-4 py-8">
                 <Searchbar />
-
                 {/* Bouton filtre */}
-                <div className="w-full max-w-7xl px-4 py-2">
+                <div className="flex w-full max-w-7xl flex-wrap items-center justify-between gap-4 px-4 py-2">
                     <button
                         className="w-[220px] cursor-pointer rounded-xl border-2 border-[#FF39B7] bg-white px-2 py-1 text-sm font-medium text-[#FF39B7]"
                         onClick={handleToggleFilters}
