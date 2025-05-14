06a8f85ff9262b300433451fb97006462705ecc9
Noha <noha.ben254@gmail.com>
2025-05-14 13:59:37 +0200

    footer

--- resources/js/components/tri/sort.jsx
+++ resources/js/components/tri/sort.jsx
@@ -1,29 +1,3 @@
-// // components/sort/index.jsx
-// import { useState } from 'react';
-// import SortArrow from '../../assets/img/sort-arrow.svg';
-
-// export default function Sort({ sort, setSort }) {
-//     const [showFilters, setShowFilters] = useState(true);
-
-//     // const [showSort, setShowSort] = useState(false);
-//     const options = ['Prix croissant', 'Prix décroissant', 'Nom A-Z', 'Nom Z-A'];
-//     const handleToggleFilters = () => {
-//         setShowFilters(!showFilters);
-//     };
-
-//     return (
-//         <>
-//             <div className="bg-[#000 flex items-center gap-2">
-//                 <img src={SortArrow} alt="Sort Arrow" className="ml-2 h-4 w-4" />
-//                 <select name="Trier par :">
-//                     <option value="ascending">Prix croissant</option>
-//                     <option value="descending">Prix décroissant</option>
-//                 </select>
-//             </div>
-//         </>
-//     );
-// }
-
 import { useState } from 'react';
 import ArrowDown from '../../assets/img/arrow-down.svg';
 import ArrowUp from '../../assets/img/arrow-up.svg';
