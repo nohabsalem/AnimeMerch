1786b7d90787c7f5f2cb6c09eb074bc4008fa7d0
Noha <noha.ben254@gmail.com>
2025-05-15 11:44:34 +0200

    footer

--- resources/js/pages/auth/register.jsx
+++ resources/js/pages/auth/register.jsx
@@ -1,6 +1,7 @@
 import { Head, useForm } from '@inertiajs/react';
 import { LoaderCircle } from 'lucide-react';
 
+import Footer from '@/components/footer';
 import InputError from '@/components/input-error';
 import TextLink from '@/components/text-link';
 import { Button } from '@/components/ui/button';
@@ -24,93 +25,104 @@ export default function Register() {
     };
 
     return (
-        <AuthLayout title="Créer un compte" description="Entrez vos informations pour créer un compte.">
-            <Head title="Register" />
-            <form className="flex flex-col gap-6 border-2 border-[#6C3989] rounded-md p-6" onSubmit={submit}>
-                <div className="grid gap-6">
-                    <div className="grid gap-2">
-                        <Label htmlFor="name" className="mb-2">Prénom :</Label>  {/* Ajout du mb-2 pour l'espacement */}
-                        <Input
-                            id="name"
-                            type="text"
-                            required
-                            autoFocus
-                            tabIndex={1}
-                            autoComplete="name"
-                            value={data.name}
-                            onChange={(e) => setData('name', e.target.value)}
-                            disabled={processing}
-                            placeholder="Prénom"
-                            className="p-2"
-                        />
-                        <InputError message={errors.name} className="mt-2" />
+        <>
+            <AuthLayout title="Créer un compte" description="Entrez vos informations pour créer un compte.">
+                <Head title="Register" />
+                <form className="flex flex-col gap-6 rounded-md border-2 border-[#6C3989] p-6" onSubmit={submit}>
+                    <div className="grid gap-6">
+                        <div className="grid gap-2">
+                            <Label htmlFor="name" className="mb-2">
+                                Prénom :
+                            </Label>{' '}
+                            {/* Ajout du mb-2 pour l'espacement */}
+                            <Input
+                                id="name"
+                                type="text"
+                                required
+                                autoFocus
+                                tabIndex={1}
+                                autoComplete="name"
+                                value={data.name}
+                                onChange={(e) => setData('name', e.target.value)}
+                                disabled={processing}
+                                placeholder="Prénom"
+                                className="p-2"
+                            />
+                            <InputError message={errors.name} className="mt-2" />
+                        </div>
+
+                        <div className="grid gap-2">
+                            <Label htmlFor="email" className="mb-2">
+                                Adresse mail :
+                            </Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                required
+                                tabIndex={2}
+                                autoComplete="email"
+                                value={data.email}
+                                onChange={(e) => setData('email', e.target.value)}
+                                disabled={processing}
+                                placeholder="email@example.com"
+                                className="p-2"
+                            />
+                            <InputError message={errors.email} />
+                        </div>
+
+                        <div className="grid gap-2">
+                            <Label htmlFor="password" className="mb-2">
+                                Mot de passe :
+                            </Label>
+                            <Input
+                                id="password"
+                                type="password"
+                                required
+                                tabIndex={3}
+                                autoComplete="new-password"
+                                value={data.password}
+                                onChange={(e) => setData('password', e.target.value)}
+                                disabled={processing}
+                                placeholder="Entrez votre mot de passe"
+                                className="p-2"
+                            />
+                            <InputError message={errors.password} />
+                        </div>
+
+                        <div className="grid gap-2">
+                            <Label htmlFor="password_confirmation" className="mb-2">
+                                Confirmation de mot de passe
+                            </Label>
+                            <Input
+                                id="password_confirmation"
+                                type="password"
+                                required
+                                tabIndex={4}
+                                autoComplete="new-password"
+                                value={data.password_confirmation}
+                                onChange={(e) => setData('password_confirmation', e.target.value)}
+                                disabled={processing}
+                                placeholder="Confirmez votre mot de passe"
+                                className="p-2"
+                            />
+                            <InputError message={errors.password_confirmation} />
+                        </div>
+
+                        <Button type="submit" className="mt-4 w-full bg-[#FF39B7] hover:bg-[#FF39B7]" tabIndex={5} disabled={processing}>
+                            {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
+                            Créer un compte
+                        </Button>
                     </div>
 
-                    <div className="grid gap-2">
-                        <Label htmlFor="email" className="mb-2">Adresse mail :</Label>
-                        <Input
-                            id="email"
-                            type="email"
-                            required
-                            tabIndex={2}
-                            autoComplete="email"
-                            value={data.email}
-                            onChange={(e) => setData('email', e.target.value)}
-                            disabled={processing}
-                            placeholder="email@example.com"
-                            className="p-2"
-                        />
-                        <InputError message={errors.email} />
+                    <div className="text-muted-foreground mt-4 text-center text-sm">
+                        Vous avez déjà un compte ?{' '}
+                        <TextLink href={route('login')} tabIndex={6} className="text-[#6C3989]">
+                            Connectez-vous.
+                        </TextLink>
                     </div>
-
-                    <div className="grid gap-2">
-                        <Label htmlFor="password" className="mb-2">Mot de passe :</Label>
-                        <Input
-                            id="password"
-                            type="password"
-                            required
-                            tabIndex={3}
-                            autoComplete="new-password"
-                            value={data.password}
-                            onChange={(e) => setData('password', e.target.value)}
-                            disabled={processing}
-                            placeholder="Entrez votre mot de passe"
-                            className="p-2"
-                        />
-                        <InputError message={errors.password} />
-                    </div>
-
-                    <div className="grid gap-2">
-                        <Label htmlFor="password_confirmation" className="mb-2">Confirmation de mot de passe</Label>
-                        <Input
-                            id="password_confirmation"
-                            type="password"
-                            required
-                            tabIndex={4}
-                            autoComplete="new-password"
-                            value={data.password_confirmation}
-                            onChange={(e) => setData('password_confirmation', e.target.value)}
-                            disabled={processing}
-                            placeholder="Confirmez votre mot de passe"
-                            className="p-2"
-                        />
-                        <InputError message={errors.password_confirmation} />
-                    </div>
-
-                    <Button type="submit" className="mt-4 w-full bg-[#FF39B7] hover:bg-[#FF39B7]" tabIndex={5} disabled={processing}>
-                        {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-                        Créer un compte
-                    </Button>
-                </div>
-
-                <div className="text-muted-foreground text-center text-sm mt-4">
-                    Vous avez déjà un compte ?{' '}
-                    <TextLink href={route('login')} tabIndex={6} className="text-[#6C3989]">
-                        Connectez-vous.
-                    </TextLink>
-                </div>
-            </form>
-
-        </AuthLayout>
+                </form>
+            </AuthLayout>
+            <Footer />
+        </>
     );
 }
