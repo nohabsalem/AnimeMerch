2c5d66075d1030c2ae4e9493f17aca46225c5c48
Noha <noha.ben254@gmail.com>
2025-05-19 11:43:22 +0200

    removal comments

--- resources/js/components/landing/our-values.jsx
+++ resources/js/components/landing/our-values.jsx
@@ -7,31 +7,10 @@ import MeganTrish from '../../assets/img/models/megan-trish.png';
 export default function OurValues() {
     const [emblaRef] = useEmblaCarousel();
 
-    // return (
-    //     <>
-    //         <div className="embla" ref={emblaRef}>
-    //             <div className="embla__container">
-    //                 <div className="embla__slide">
-    //                     <img src={MeganStand} alt="Mannequin 1" />
-    //                 </div>
-    //                 <div className="embla__slide">
-    //                     <img src={MeganTrish} alt="Mannequin 2" />
-    //                 </div>
-    //                 <div className="embla__slide">
-    //                     <img src={MeganJolyne} alt="Mannequin 3" />
-    //                 </div>
-    //                 <div className="embla__slide">
-    //                     <img src={MeganBruno} alt="Mannequin 4" />
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </>
-    // );
-
     return (
         <>
             <main className="flex flex-row items-center justify-center gap-4 px-6 py-8">
-                <div className="w-1/2">
+                <div className="w-1/2 bg-[#ff11ff]">
                     <div className="embla" ref={emblaRef}>
                         <div className="embla__container">
                             <div className="embla__slide">
@@ -49,9 +28,9 @@ export default function OurValues() {
                         </div>
                     </div>
                 </div>
-                <div className="flex w-1/2 flex-col justify-center bg-white p-8">
+                <div className="flex w-1/2 flex-col justify-center bg-[#ff11ff]">
                     <h2 className="mb-4 text-lg font-bold">Titre accrocheur</h2>
-                    <p>Voici un petit paragraphe de texte à côté de l’image. Tu peux ajouter des boutons, des listes ou ce que tu veux ici.</p>
+                    <p>Voici un petit paragraphe de texte à côté de l’image. </p>
                 </div>
             </main>
         </>
