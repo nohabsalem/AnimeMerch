95b852bea8bedc8c1efb05db9e27239837dbff72
Noha <noha.ben254@gmail.com>
2025-05-21 15:52:56 +0200

    const nav

--- resources/js/components/footer.jsx
+++ resources/js/components/footer.jsx
@@ -1,28 +1,11 @@
 import logo from '../assets/img/jojo-logo.png';
 const navigation = {
-    solutions: [
-        { name: 'Marketing', href: '' },
-        { name: 'Analytics', href: '' },
-        { name: 'Automation', href: '' },
-        { name: 'Commerce', href: '' },
-        { name: 'Insights', href: '' },
-    ],
-    support: [
-        { name: 'Submit ticket', href: '' },
-        { name: 'Documentation', href: '' },
-        { name: 'Guides', href: '' },
-    ],
-    company: [
-        { name: 'About', href: '' },
-        { name: 'Blog', href: '' },
-        { name: 'Jobs', href: '' },
-        { name: 'Press', href: '' },
-    ],
-    legal: [
+    Mentions: [
         { name: 'Politique de Confidentialité', href: '/privacy-policy' },
         { name: 'Conditions de Vente', href: '/terms-of-sale' },
-        { name: 'License', href: '' },
     ],
+    Support: [{ name: 'Nous contacter', href: '' }],
+    Entreprise: [{ name: 'À propos de nous', href: '/about-us' }],
 };
 
 export default function Footer() {
@@ -30,25 +13,13 @@ export default function Footer() {
         <footer className="bg-[#6C3989]">
             <div className="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8 lg:py-32">
                 <div className="xl:grid xl:grid-cols-3 xl:gap-8">
-                    <img alt="Company name" src={logo} className="h-9" />
+                    <img alt="Entreprise name" src={logo} className="h-9" />
                     <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
                         <div className="md:grid md:grid-cols-2 md:gap-8">
-                            <div>
-                                <h3 className="text-md text-sm/6 font-semibold text-white">Solutions</h3>
-                                <ul role="list" className="mt-6 space-y-4">
-                                    {navigation.solutions.map((item) => (
-                                        <li key={item.name}>
-                                            <a href={item.href} className="text-sm/6 text-white">
-                                                {item.name}
-                                            </a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
                             <div className="mt-10 md:mt-0">
                                 <h3 className="text-md text-sm/6 font-semibold text-white">Support</h3>
                                 <ul role="list" className="mt-6 space-y-4">
-                                    {navigation.support.map((item) => (
+                                    {navigation.Mentions.map((item) => (
                                         <li key={item.name}>
                                             <a href={item.href} className="text-md text-sm/6 text-white hover:text-white">
                                                 {item.name}
@@ -60,9 +31,9 @@ export default function Footer() {
                         </div>
                         <div className="md:grid md:grid-cols-2 md:gap-8">
                             <div>
-                                <h3 className="text-md text-sm/6 font-semibold text-white">Company</h3>
+                                <h3 className="text-md text-sm/6 font-semibold text-white">Entreprise</h3>
                                 <ul role="list" className="mt-6 space-y-4">
-                                    {navigation.company.map((item) => (
+                                    {navigation.Entreprise.map((item) => (
                                         <li key={item.name}>
                                             <a href={item.href} className="text-md text-sm/6 text-white hover:text-white">
                                                 {item.name}
@@ -72,9 +43,9 @@ export default function Footer() {
                                 </ul>
                             </div>
                             <div className="mt-10 md:mt-0">
-                                <h3 className="text-md text-sm/6 font-semibold text-white">Legal</h3>
+                                <h3 className="text-md text-sm/6 font-semibold text-white">Mentions</h3>
                                 <ul role="list" className="mt-6 space-y-4">
-                                    {navigation.legal.map((item) => (
+                                    {navigation.Support.map((item) => (
                                         <li key={item.name}>
                                             <a href={item.href} className="text-md text-sm/6 text-white hover:text-white">
                                                 {item.name}
