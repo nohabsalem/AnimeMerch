a5ec28f4869473ee53f28be74b713778e4afff24
Noha <noha.ben254@gmail.com>
2025-05-22 16:34:54 +0200

    new (test)

--- resources/js/pages/welcome.jsx
+++ resources/js/pages/welcome.jsx
@@ -1,11 +1,10 @@
-import { Head, Link } from '@inertiajs/react';
-import Banner from '../assets/img/jojo-banner.jpg';
-
+import { Head } from '@inertiajs/react';
 import Footer from '../components/footer';
 import Header from '../components/header';
-import ProductCards from './products/cards';
 
-import Carrousel from '../components/landing/carrousel';
+import { Link } from '@inertiajs/react';
+import JosukeBg from '../assets/img/josuke-wallpaper.jpg';
+import Square from '../assets/img/square.png';
 export default function Welcome({ phpVersion }) {
     return (
         <>
@@ -14,22 +13,26 @@ export default function Welcome({ phpVersion }) {
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
             <Header />
+            <img src={JosukeBg} alt="Josuke" className="full w-full object-cover" />
+            <Link href="/products" className="border-2 border-black hover:text-red-800">
+                Shopper
+            </Link>
+            {/* <main className="flex flex-col items-center justify-center gap-4 px-6 py-8"> */}
 
-            <div className="flex w-full items-center justify-center">
-                <img src={Banner} className="max-h-[350px] w-full object-cover" alt="Bannière" />
-            </div>
-
-            <main className="flex flex-col items-center justify-center gap-4 px-6 py-8">
-                <h1 className="text-4xl font-bold text-[#6C3989]">Bienvenue sur AnimeMerch</h1>
-                <p className="text-lg text-gray-700">Votre destination ultime pour les produits dérivés d'animés !</p>
-
-                <button className="mt-4 rounded-xl bg-[#FF39B7] px-10 py-2 text-white">
-                    <Link href="/products">Shopper</Link>
-                </button>
-                <ProductCards />
-                <Carrousel />
+            <main className="flex flex-row items-center justify-center py-8">
+                <div className="flex w-full flex-col justify-center bg-blue-700 px-2 md:w-1/2 md:px-4">
+                    <p>Image 1</p>
+                    <img src={Square} alt="Square" className="w-1/2" />
+                </div>
+                <div className="flex w-full flex-col justify-center bg-red-700 px-2 md:w-1/2 md:px-4">
+                    <p>Image 2</p>
+                    <img src={Square} alt="Square" className="w-1/2" />
+                </div>
             </main>
-
+            <div className="flex w-full bg-green-700">
+                <p>Image 3</p>
+                <img src={Square} alt="Square" />
+            </div>
             <Footer />
         </>
     );
