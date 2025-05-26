4166dfb5d85d34390a1c1aa4c3d8a664abc55cf2
Noha <noha.ben254@gmail.com>
2025-05-26 11:52:06 +0200

    logo

--- resources/js/components/app-logo.jsx
+++ resources/js/components/app-logo.jsx
@@ -1,12 +1,16 @@
-import AppLogoIcon from './app-logo-icon';
-
+import { Link } from '@inertiajs/react';
+import Logo from '../assets/img/jojo-logo.png';
+// import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <>
             <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                <Link href="/">
+                    <img src={Logo} alt="Logo" className="w-auto" />
+                </Link>
+                {/* <AppLogoIcon className="size-5 fill-current text-white dark:text-black" /> */}
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
+            <div className="ml-1 grid flex-1 text-left text-sm text-[#6C3989]">
                 <span className="mb-0.5 truncate leading-none font-semibold">Anime Merch</span>
             </div>
         </>
