889d82f8ae651ab16eb81075dd43a9b0446caea4
Noha <noha.ben254@gmail.com>
2025-05-26 14:35:25 +0200

    link

--- resources/js/pages/errors/404.jsx
+++ resources/js/pages/errors/404.jsx
@@ -11,7 +11,7 @@ export default function NotFound() {
                 <h1 className="text-6xl font-bold text-white drop-shadow-lg">Oups ! Cette page n'existe pas.</h1>
                 <p className="mt-4 text-lg text-white drop-shadow">Vous pouvez toujours vous rendre à l'accueil.</p>
                 <button className="mt-4 rounded-xl bg-[#FF39B7] px-10 py-2 text-white">
-                    <Link href="/products">Accueil</Link>
+                    <Link href="/">Accueil</Link>
                 </button>
             </div>
         </>
