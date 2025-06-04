238c7622b9c59ab310d85b6b4a829e81ac7cf383
Noha <noha.ben254@gmail.com>
2025-06-04 13:16:02 +0200

    order of cards

--- resources/js/pages/admin/dashboard.jsx
+++ resources/js/pages/admin/dashboard.jsx
@@ -25,7 +25,7 @@ export default function AdminDashboard() {
                     {/* Statistiques */}
                     <div className="grid grid-cols-2 gap-4 sm:grid-cols-4">
                         {stats.map((item) => (
-                            <div key={item.name} className="rounded-xl border-2 border-[#FF39B7] bg-white p-4 shadow-md">
+                            <div key={item.name} className="align-items-center rounded-xl border-2 border-[#FF39B7] bg-white p-4 shadow-md">
                                 <span className="text-md block truncate font-medium text-[#6C3989]">{item.name}</span>
                                 <span className="mt-2 block text-2xl font-bold text-[#FF39B7]">{item.number}</span>
                             </div>
@@ -34,16 +34,17 @@ export default function AdminDashboard() {
 
                     <div className="rounded-xl border border-[#E5E7EB] bg-white p-4">
                         <h2 className="mb-4 text-xl font-semibold text-[#6c3989]">Accès rapide</h2>
+
                         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
                             <div className="flex items-start gap-3 rounded-lg border border-[#F3F4F6] bg-white p-3 shadow-sm">
                                 <div className="flex h-8 w-8 items-center justify-center rounded-md bg-[#E6D8F4]">
-                                    <BuildingStorefrontIcon className="h-4 w-4 text-[#6C3989]" />
+                                    <UserIcon className="h-6 w-6 text-[#6C3989]" />
                                 </div>
                                 <div>
-                                    <Link href="/products-view" className="text-md block font-semibold text-[#6c3989]">
-                                        Produits
+                                    <Link href="/users-list" className="text-md block font-semibold text-[#6c3989]">
+                                        Utilisateurs
                                     </Link>
-                                    <p className="text-sm text-gray-600">Consulter la liste les produits.</p>
+                                    <p className="text-sm text-gray-600"> Voir et gérer les utilisateurs.</p>
                                 </div>
                             </div>
                             <div className="flex items-start gap-3 rounded-lg border border-[#F3F4F6] bg-white p-3 shadow-sm">
@@ -60,6 +61,18 @@ export default function AdminDashboard() {
 
                             <div className="flex items-start gap-3 rounded-lg border border-[#F3F4F6] bg-white p-3 shadow-sm">
                                 <div className="flex h-8 w-8 items-center justify-center rounded-md bg-[#E6D8F4]">
+                                    <BuildingStorefrontIcon className="h-4 w-4 text-[#6C3989]" />
+                                </div>
+                                <div>
+                                    <Link href="/products-view" className="text-md block font-semibold text-[#6c3989]">
+                                        Produits
+                                    </Link>
+                                    <p className="text-sm text-gray-600">Consulter la liste les produits.</p>
+                                </div>
+                            </div>
+
+                            <div className="flex items-start gap-3 rounded-lg border border-[#F3F4F6] bg-white p-3 shadow-sm">
+                                <div className="flex h-8 w-8 items-center justify-center rounded-md bg-[#E6D8F4]">
                                     <ArchiveBoxArrowDownIcon className="h-6 w-6 text-[#6C3989]" />
                                 </div>
                                 <div>
@@ -69,18 +82,6 @@ export default function AdminDashboard() {
                                     <p className="text-sm text-gray-600"> Voir et gérer les produits.</p>
                                 </div>
                             </div>
-                            <div className="flex items-start gap-3 rounded-lg border border-[#F3F4F6] bg-white p-3 shadow-sm">
-                                <div className="flex h-8 w-8 items-center justify-center rounded-md bg-[#E6D8F4]">
-                                    <UserIcon className="h-6 w-6 text-[#6C3989]" />
-                                </div>
-                                <div>
-                                    <Link href="/users-list" className="text-md block font-semibold text-[#6c3989]">
-                                        Utilisateurs
-                                    </Link>
-                                    <p className="text-sm text-gray-600"> Voir et gérer les utilisateurs.</p>
-                                </div>
-                            </div>
-                            <p>commit p</p>
                         </div>
                     </div>
                 </div>
