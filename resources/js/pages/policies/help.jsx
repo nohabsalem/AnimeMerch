e1ce2ced7649c2179dd2a80966b04622c80eace7
Noha <noha.ben254@gmail.com>
2025-06-05 11:00:11 +0200

    bg

--- resources/js/pages/policies/help.jsx
+++ resources/js/pages/policies/help.jsx
@@ -1,34 +1,39 @@
-import { Link } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
+import Gyro from '../../assets/img/gyro.jpg';
 import Footer from '../../components/footer';
+
 export default function Aide() {
     return (
         <>
-            <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-br from-pink-100 via-white to-blue-100 px-4">
-                <div className="max-w-md rounded-2xl bg-white p-8 text-center shadow-xl">
-                    <h1 className="mb-4 text-3xl font-extrabold text-gray-800">Page d’aide</h1>
-                    <p className="mb-2 text-gray-600">Si vous avez besoin d’assistance, n’hésitez pas à contacter notre équipe de support.</p>
-                    <p className="mb-2 text-gray-600">
-                        Vous pouvez nous écrire à : <br />
-                        <a href="mailto:animemerch-aide@exemple.com" className="text-blue-600 hover:underline">
-                            <i>Animemerch-aide@exemple.com</i>
-                        </a>
-                    </p>
-                    <p className="mb-2 text-gray-600">
-                        Ou visitez notre{' '}
-                        <a href="/faq" className="text-blue-600 hover:underline">
-                            page FAQ
-                        </a>{' '}
-                        pour les questions fréquentes.
-                    </p>
-                    <p className="mb-1 text-gray-600">Merci pour votre patience !</p>
-                    <p className="mb-1 text-gray-600">Nous sommes là pour vous aider.</p>
-                    <p className="mb-4 text-gray-600">Votre satisfaction est notre priorité.</p>
+            <Head title="Aide" />
+            <div className="flex min-h-screen w-full items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url(${Gyro})` }}>
+                <div className="flex h-screen flex-col items-center justify-center">
+                    <div className="max-w-md rounded-2xl bg-white p-8 text-center shadow-xl">
+                        <h1 className="mb-4 text-3xl font-extrabold text-gray-800">Page d’aide</h1>
+                        <p className="mb-2 text-gray-600">Si vous avez besoin d’assistance, n’hésitez pas à contacter notre équipe de support.</p>
+                        <p className="mb-2 text-gray-600">
+                            Vous pouvez nous écrire à : <br />
+                            <a href="mailto:animemerch-aide@exemple.com" className="text-blue-600 hover:underline">
+                                <i>Animemerch-aide@exemple.com</i>
+                            </a>
+                        </p>
+                        <p className="mb-2 text-gray-600">
+                            Ou visitez notre{' '}
+                            <a href="/faq" className="text-blue-600 hover:underline">
+                                page FAQ
+                            </a>{' '}
+                            pour les questions fréquentes.
+                        </p>
+                        <p className="mb-1 text-gray-600">Merci pour votre patience !</p>
+                        <p className="mb-1 text-gray-600">Nous sommes là pour vous aider.</p>
+                        <p className="mb-4 text-gray-600">Votre satisfaction est notre priorité.</p>
 
-                    <Link href="/">
-                        <button className="text-600 mt-2 cursor-pointer rounded-xl bg-[#FF39B7] px-6 py-2 font-semibold text-white">
-                            Retour à l’accueil
-                        </button>
-                    </Link>
+                        <Link href="/">
+                            <button className="text-600 mt-2 cursor-pointer rounded-xl bg-[#FF39B7] px-6 py-2 font-semibold text-white">
+                                Retour à l’accueil
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
             <Footer />
