b2aee92182a6d245236ebde99f3cc5ab07532a88
Noha <noha.ben254@gmail.com>
2025-06-11 16:45:19 +0200

    removal of bg

--- resources/js/components/admin/sort.jsx
+++ resources/js/components/admin/sort.jsx
@@ -8,7 +8,7 @@ export default function Sort() {
     return (
         <Menu as="div" className="relative inline-block text-left">
             <div>
-                <MenuButton className="inline-flex w-full cursor-pointer justify-center gap-x-1.5 rounded-xl border-2 border-[#6C3989] bg-white px-2 py-1 text-sm font-medium text-[#6C3989] shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50">
+                <MenuButton className="inline-flex w-full cursor-pointer justify-center gap-x-1.5 rounded-xl border-2 border-[#6C3989] px-2 py-1 text-sm font-medium text-[#6C3989] shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50">
                     <img src={Arrows} height={15} width={15} />
                     Trier par
                     <ChevronDownIcon aria-hidden="true" className="-mr-1 size-5" />
@@ -17,7 +17,7 @@ export default function Sort() {
 
             <MenuItems
                 transition
-                className="absolute left-0 z-10 mt-2 w-56 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                className="absolute left-0 z-10 mt-2 w-56 origin-top-left rounded-md shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
             >
                 <div className="py-1">
                     <MenuItem
