3a831896b5537e379c8c91bc6f7abdddc2b71244
Noha <noha.ben254@gmail.com>
2025-06-11 10:04:39 +0200

    a into link

--- resources/js/pages/toto.jsx
+++ resources/js/pages/toto.jsx
@@ -55,7 +55,7 @@ export default function Toto({ phpVersion }) {
                                     </span>
                                     <span>
                                         Read the
-                                        <a
+                                        <Link
                                             href="https://laravel.com/docs"
                                             target="_blank"
                                             className="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
@@ -75,7 +75,7 @@ export default function Toto({ phpVersion }) {
                                                     strokeLinecap="square"
                                                 />
                                             </svg>
-                                        </a>
+                                        </Link>
                                     </span>
                                 </li>
                                 <li className="relative flex items-center gap-4 py-2 before:absolute before:top-0 before:bottom-1/2 before:left-[0.4rem] before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A]">
@@ -86,7 +86,7 @@ export default function Toto({ phpVersion }) {
                                     </span>
                                     <span>
                                         Watch video tutorials at
-                                        <a
+                                        <Link
                                             href="https://laracasts.com"
                                             target="_blank"
                                             className="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
@@ -106,19 +106,19 @@ export default function Toto({ phpVersion }) {
                                                     strokeLinecap="square"
                                                 />
                                             </svg>
-                                        </a>
+                                        </Link>
                                     </span>
                                 </li>
                             </ul>
                             <ul className="flex gap-3 text-sm leading-normal">
                                 <li>
-                                    <a
+                                    <Link
                                         href="https://cloud.laravel.com"
                                         target="_blank"
                                         className="inline-block rounded-sm border border-black bg-[#1b1b18] px-5 py-1.5 text-sm leading-normal text-white hover:border-black hover:bg-black dark:border-[#eeeeec] dark:bg-[#eeeeec] dark:text-[#1C1C1A] dark:hover:border-white dark:hover:bg-white"
                                     >
                                         Deploy now
-                                    </a>
+                                    </Link>
                                 </li>
                             </ul>
                         </div>
