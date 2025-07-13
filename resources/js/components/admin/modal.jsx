81b7c00056b0a9914d0869f6b8a40fcbf86a2326
Noha <noha.ben254@gmail.com>
2025-07-13 19:57:00 +0200

    css

--- resources/js/components/admin/modal.jsx
+++ resources/js/components/admin/modal.jsx
@@ -1,12 +1,11 @@
-// components/Modal.jsx
 export default function Modal({ open, onClose, children }) {
     if (!open) return null;
     return (
-        <div className="bg-opacity-50 fixed inset-0 z-50 flex items-center justify-center bg-black/10 backdrop-blur-sm">
-            <div className="relative w-full max-w-lg rounded-lg bg-white p-6 shadow-lg">
+        <div className="bg-opacity-50 fixed inset-0 z-50 flex items-center justify-center bg-black/10 p-4 backdrop-blur-sm">
+            <div className="relative max-h-[90vh] w-full max-w-lg overflow-auto rounded-lg bg-white p-6 shadow-lg sm:max-w-md md:max-w-lg">
                 <button
                     onClick={onClose}
-                    className="absolute top-2 right-2 cursor-pointer rounded-lg bg-red-600 px-4 py-2 text-2xl text-white"
+                    className="absolute top-2 right-2 cursor-pointer rounded-lg bg-red-600 px-4 py-2 text-2xl text-white transition hover:bg-red-700"
                     aria-label="Fermer"
                 >
                     &times;
