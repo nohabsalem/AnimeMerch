dc0d967f24ad5170d388b0ba70bf68b0f8fc71ae
Noha <noha.ben254@gmail.com>
2025-07-14 20:22:43 +0200

    product create

--- routes/web.php
+++ routes/web.php
@@ -99,10 +99,11 @@ Route::get('/payment', function () {
 
 
 // Traitement du formulaire de création
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
 Route::post('/products', [ProductController::class, 'store'])->name('products.store');
 
 // Liste des produits (affichage dynamique)
-Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 
 // Détail d’un produit
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
