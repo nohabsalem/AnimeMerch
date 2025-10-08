3fd84d432b66f2f8db4b430cbb4d8a4f4222d4a0
Noha <noha.ben254@gmail.com>
2025-10-08 10:49:42 +0200

    translated in french

--- resources/js/pages/auth/login.jsx
+++ resources/js/pages/auth/login.jsx
@@ -82,7 +82,7 @@ export default function Login({ status, canResetPassword }) {
 
                             <Button type="submit" className="mt-4 w-full bg-[#FF39B7] hover:bg-[#FF39B7]" tabIndex={4} disabled={processing}>
                                 {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-                                Log in
+                                Se connecter
                             </Button>
                         </div>
 
