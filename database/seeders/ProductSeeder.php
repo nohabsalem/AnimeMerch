5ee1e9d48a86f50e1c8026e9561b4b783b5706bd
Noha <noha.ben254@gmail.com>
2025-10-29 12:49:49 +0100

    seeders functionning

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,26 +4,49 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Product;
-
+use App\Models\Variant;
+//  
 class ProductSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        Product::create([
-            'name' => 'Seeder1',
-            'price' => 19.99,
+        $mainImage = 'img/shirts/chemise-beige-dos.jpg';
+
+        $product = Product::create([
+            'name' => 'My first seeder ',
             'description' => 'congratulations ! you ran a seeder',
+            'image_path' => $mainImage,
+            'category_id' => null,
+            'price' => 19.99,
             'stock' => 100,
         ]);
 
-        Product::create([
-            'name' => 'Seeder2',
-            'price' => 19.99,
-            'description' => 'congratulations ! you ran a seeder',
-            'stock' => 100,
-        ]);
+        $sizes = [
+            ['size' => 'S', 'stock' => 12],
+            ['size' => 'M', 'stock' => 30],
+            ['size' => 'L', 'stock' => 48]
+        ];
+
+        foreach ($sizes as $variant) {
+            Variant::create([
+                'product_id' => $product->id,
+                'size' => $variant['size'],
+                'stock' => $variant['stock'],
+            ]);
+        }
+
+        // $images = [
+        //     'img/shirts/chemise-beige-dos.jpg',
+        //     'img/shirts/chemise-beige.jpg',
+        //     'img/shirts/chemise-bucciarati-vue-dos.jpg',
+        //     'img/shirts/chemise-bucciarati.jpg'
+        // ];
+
+        // foreach ($images as $imgPath) {
+        //     ProductImage::create([
+        //         'product_id' => $product->id,
+        //         'image_path' => $imgPath,
+        //     ]);
+        // }
     }
 }
