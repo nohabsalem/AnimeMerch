e3ff2159a9b378095526af8b5277dca70e766ff8
Noha <noha.ben254@gmail.com>
2025-10-29 12:50:00 +0100

    user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -10,7 +11,11 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ProductSeeder::class,
-            // autres seeders si besoin
+        ]);
+
+        User::factory()->create([
+            'name' => 'testing user',
+            'email' => 'test@example.com',
         ]);
     }
 }
