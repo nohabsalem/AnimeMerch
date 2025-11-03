ffa835778b903b0b2190c03255c479ab4faafd30
Noha <noha.ben254@gmail.com>
2025-11-03 13:15:09 +0100

    add Category model and refactor Product model structure

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -15,6 +15,28 @@ class Product extends Model
         'stock',
         'image_path'
     ];
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function variants()
+    {
+        return $this->hasMany(Variant::class);
+    }
+} -->*/
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'stock', 'image_path'];
 
     public function images()
     {
